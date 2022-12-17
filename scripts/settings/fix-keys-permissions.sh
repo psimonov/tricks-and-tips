1ffc591021e1237f789c28e995c4c3c8337c92cc
Pavel Simonov <psimonov.web@gmail.com>
2022-12-17 05:11:34 +0300

    fix user for scripts/settings/fix-keys-permissions

--- scripts/settings/fix-keys-permissions.sh
+++ scripts/settings/fix-keys-permissions.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-################################################# EXECUTE THIS! #############################################################################
-#                                                                                                                                           #
-#   sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/psimonov/tricks-and-tips/master/scripts/settings/fix-keys-permissions.sh)"   #
-#                                                                                                                                           #
-#############################################################################################################################################
+################################################# EXECUTE THIS! ########################################################################
+#                                                                                                                                 #
+#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/psimonov/tricks-and-tips/master/scripts/settings/fix-keys-permissions.sh)"   #
+#                                                                                                                                 #
+########################################################################################################################################
 
-chmod 600 -R ~/.ssh
-chmod 644 ~/.ssh/known_hosts
-chmod 755 ~/.ssh
+sudo chmod 600 -R ~/.ssh
+sudo chmod 644 ~/.ssh/known_hosts
+sudo chmod 755 ~/.ssh
